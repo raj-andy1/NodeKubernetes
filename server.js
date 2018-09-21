b08632290f26152db0921360d3d6269c53e5b960
Andy Rajagopalan <andyr@Andys-MacBook-Pro.local>
2018-09-21 11:35:35 -0700

    added Harish

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 let userRouter = express.Router()
 
 userRouter.get('*', (req, res) => {
-	res.status(200).json({hello: 'Yahoo!! Today is the day'})
+	res.status(200).json({hello: 'Harish!! Things are great'})
 });
 
 app.use(userRouter)
